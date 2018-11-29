a7af5102ec67a5785a3b926804da510248643043
Tom Gates <thomas.gates@shopify.com>
2018-11-28 19:42:43 -0500

    Basic sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  
+
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :email, presence: true, 
-                    length: { maximum: 255 }, 
-                    format: { with: VALID_EMAIL_REGEX },
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX, message: 'does not look like an email' },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 8 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users, only: [:show]
 end
